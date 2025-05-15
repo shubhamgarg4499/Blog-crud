388be15651217057b6bee256f20d112465deb88c
Shubham garg <shubhamgarg10904@gmail.com>
2025-05-15 16:46:09 +0530

    updated backend of blog-app

--- routes/Blog.routes.js
+++ routes/Blog.routes.js
@@ -1,6 +1,6 @@
 
 const BlogRouter = require("express").Router()
-const { createBlog, getAllBlog, deletePost, getpostbyid, editBlog, changeFeaturedImage } = require("../controllers/Blog.controller")
+const { createBlog, getAllBlog, deletePost, getpostbyid, editBlog } = require("../controllers/Blog.controller")
 const uploadOnCloudinary = require("../middlewares/Cloudinary.middleware");
 const upload = require("../others/Multer.setup");
 BlogRouter.route("/create-blog").post(upload.array("Fimage", 1), uploadOnCloudinary, createBlog)
@@ -8,5 +8,4 @@ BlogRouter.route("/getAllBlog").get(getAllBlog)
 BlogRouter.route("/deletePost/:id").delete(deletePost)
 BlogRouter.route("/getBlog/:id").get(getpostbyid)
 BlogRouter.route("/editBlog/:id").post(editBlog)
-BlogRouter.route("/changeFeaturedImage/:id").post(upload.array("fimage", 1), uploadOnCloudinary, changeFeaturedImage)
 module.exports = BlogRouter
